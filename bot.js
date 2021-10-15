0d6f5edc9ca857c7459ebf83c4d3181a60d5fe70
42win <55066281+42win@users.noreply.github.com>
2021-10-16 00:13:37 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,19 @@
 const { Telegraf } = require('telegraf')
 
-const bot = new Telegraf('2026730005:AAH7FEMPq8POyRVa7iDWqe04XHnKKUPUYvo')
+const bot = new Telegraf('tokentelegram')
 const date_ob = new Date();
 
 const mysql = require('mysql');
 
 var dataSts = {}
 var dateCek = {}
-  
-// const conn = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'test'
-// }) 
+ 
 
 const conn = mysql.createConnection({
-    host: '10.97.3.212',
-    user: 'user_ync',
-    password: 'yncpamasuka2019#',
-    database: 'digipos_revamp_v2'
+    host: '',
+    user: '',
+    password: '',
+    database: ''
 })
  
 conn.connect(function(err){
@@ -139,15 +133,12 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
         let date = year+month;
         
         var sql1 = "select * from data_do.DO_AREA4_"+date+" limit 1" ;
-        // var sql2 = "select * from digipos_revamp_v2.report_sellthru_"+date+" limit 1" ;
         var sql3 = "select * from digipos_revamp_v2.report_outlet_reference_"+date+" limit 1" ;
         
         dataSts['data1']  = await sqlCek(sql1,date);
-        // dataSts['data2']  = await sqlCek(sql2,date);
         dataSts['data3']  = await sqlCek(sql3,date);
         
         dateCek['date1']  = await getDate5(dataSts['data1']);
-        // dateCek['date2']  = await getDate5(dataSts['data2']);
 
         var dateCek1 = new Array(
             dateCek['date1'].date1,
@@ -156,14 +147,6 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
             dateCek['date1'].date4,
             dateCek['date1'].date5
         )
-        
-        // var dateCek2 = new Array(
-        //     dateCek['date2'].date1,
-        //     dateCek['date2'].date2,
-        //     dateCek['date2'].date3,
-        //     dateCek['date2'].date4,
-        //     dateCek['date2'].date5
-        // )
   
         console.log("start setion 1")
 
@@ -187,38 +170,17 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
         if(dataSts['data5'] !== null){  
             console.log("data ditemukan");
 
-             //cek tbl 2  
-            // for (let i = 0; i < 5; i++) {    
-
-            //     console.log(dateCek2[i])  
-            //     var dtFix2 = dateCek2[i] 
-            //     var sql_cek = "select serial_number from (select * from data_do.DO_AREA4_"+dtFix1+" where MSISDN_CODE="+dataInput+" limit 1)a LEFT JOIN (  select serial_number from digipos_revamp_v2.report_sellthru_"+dateCek2[i]+"  ) b ON  serial_number=MSISDN_CODE  " ;
-            //     dataSts['data4']  = await sqlCek1(sql_cek); 
-
-            //     if(dataSts['data4'] !== null){  
-            //         break;   
-            //     } 
-            // }  
-    
-            console.log(dtFix2)  
         
             var sql = "SELECT " + 
                 "msisdn_code as No_seri_vf,PRODUCT_CODE as kode_produk, DIST_DATE as tgl_distribusi, " +
 
                 "Nama_SBP, Cluster,c.Branch, Regional "+
-                
-                // ", tgl_sellthru, sf_sellthru, id_outlet_st, " +
-
-                // "no_rs,nama_outlet,kelurahan,kecamatan, kota "+
 
                 "from (select * from data_do.DO_AREA4_"+dtFix1+" where MSISDN_CODE="+dataInput+" limit 1)a " +
-
-                // "LEFT JOIN (select serial_number,created_date as tgl_sellthru, outlet_id as id_outlet_st,sf_code as sf_sellthru  from digipos_revamp_v2.report_sellthru_"+dtFix2+" ) b ON serial_number=MSISDN_CODE " +
                 
                 "LEFT JOIN (select `AD Code` as AD_Code1,`Nama AD` as Nama_SBP,Cluster,Branch, Regional from test.refference_code_AD_nasional) c ON AD_Code=AD_Code1 "; 
                 
-                // "LEFT JOIN (select no_rs,outlet_id as otl_id, nama_outlet, kelurahan,kecamatan, kota from digipos_revamp_v2.report_outlet_reference_"+dataSts['data3']+")d ON otl_id=id_outlet_st  limit 1 ";
-
+              
             conn.query(sql, function (err, result, fields){
 
                 console.log(sql);
@@ -239,14 +201,6 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
                         dataResult += 'Cluster : '+item.Cluster+'\n',
                         dataResult += 'Branch : '+item.Branch+'\n',
                         dataResult += 'Regional : '+item.Regional+'\n'
-                        // dataResult += 'tgl_sellthru : '+item.tgl_sellthru+'\n',
-                        // dataResult += 'sf_sellthru : '+item.sf_sellthru+'\n',
-                        // dataResult += 'id_outlet_st : '+item.id_outlet_st+'\n',
-                        // dataResult += 'no_rs : '+item.no_rs+'\n',
-                        // dataResult += 'nama_outlet : '+item.nama_outlet+'\n',
-                        // dataResult += 'kelurahan : '+item.kelurahan+'\n',
-                        // dataResult += 'kecamatan : '+item.kecamatan+'\n',
-                        // dataResult += 'kota : '+item.kota+'\n' 
                     })  
      
                     result = dataResult 
@@ -286,15 +240,10 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
         let date = year+month;
 
         var sql1 = "select * from data_do.DO_AREA4_"+date+" limit 1" ;
-        // var sql2 = "select * from digipos_revamp_v2.report_sellthru_"+date+" limit 1" ;
         var sql3 = "select * from digipos_revamp_v2.report_outlet_reference_"+date+" limit 1" ;
 
         dataSts['data1']  = await sqlCek(sql1,date);
-        // dataSts['data2']  = await sqlCek(sql2,date);
-        // dataSts['data3']  = await sqlCek(sql3,date);
-
         dateCek['date1']  = await getDate5(dataSts['data1']);
-        // dateCek['date2']  = await getDate5(dataSts['data2']);
 
         var dateCek1 = new Array(
             dateCek['date1'].date1,
@@ -303,14 +252,7 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
             dateCek['date1'].date4,
             dateCek['date1'].date5
         )
-        
-        // var dateCek2 = new Array(
-        //     dateCek['date2'].date1,
-        //     dateCek['date2'].date2,
-        //     dateCek['date2'].date3,
-        //     dateCek['date2'].date4,
-        //     dateCek['date2'].date5
-        // )
+       
     
         console.log("start setion 1")
 
@@ -334,39 +276,18 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
 
         if(dataSts['data5'] !== null){  
             console.log("data ditemukan");
-              // cek tbl 2
-            //   for (let i = 0; i < 5; i++) {    
 
-            //     console.log(dateCek2[i])  
-            //     var dtFix2 = dateCek2[i] 
-    
-            //     var sql_cek = "select serial_number from (select * from data_do.DO_AREA4_"+dtFix1+" where MSISDN="+dataInput+" limit 1)a LEFT JOIN (  select serial_number,msisdn as msisdn1 from digipos_revamp_v2.report_sellthru_"+dateCek2[i]+"  ) b ON msisdn1=msisdn " ;
-            //     dataSts['data4']  = await sqlCek1(sql_cek); 
-    
-            //     if(dataSts['data4'] !== null){  
-            //         break;   
-            //     } 
-            // }  
-
-            // console.log(dtFix2)  
 
             var sql = "SELECT " + 
             "msisdn_code as No_seri_vf,PRODUCT_CODE as kode_produk, DIST_DATE as tgl_distribusi, " +
 
             "Nama_SBP, Cluster,c.Branch, Regional " +
-            
-            // ", tgl_sellthru, sf_sellthru, id_outlet_st, " +
-
-            // "no_rs,nama_outlet,kelurahan,kecamatan, kota "+
 
             "from (select * from data_do.DO_AREA4_"+dtFix1+" where MSISDN="+dataInput+" limit 1)a " +
 
-            // "LEFT JOIN (select msisdn as msisdn1,created_date as tgl_sellthru, outlet_id as id_outlet_st,sf_code as sf_sellthru  from digipos_revamp_v2.report_sellthru_"+dtFix2+" ) b ON msisdn1=msisdn " +
-            
             "LEFT JOIN (select `AD Code` as AD_Code1,`Nama AD` as Nama_SBP,Cluster,Branch, Regional from test.refference_code_AD_nasional) c ON AD_Code=AD_Code1 ";
             
-            // "LEFT JOIN (select no_rs,outlet_id as otl_id, nama_outlet, kelurahan,kecamatan, kota from digipos_revamp_v2.report_outlet_reference_"+dataSts['data3']+")d ON otl_id=id_outlet_st limit 1 ";
-
+          
             conn.query(sql, function (err, result, fields){
 
                 console.log(sql);
@@ -387,14 +308,7 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
                         dataResult += 'Cluster : '+item.Cluster+'\n',
                         dataResult += 'Branch : '+item.Branch+'\n',
                         dataResult += 'Regional : '+item.Regional+'\n'
-                        // dataResult += 'tgl_sellthru : '+item.tgl_sellthru+'\n',
-                        // dataResult += 'sf_sellthru : '+item.sf_sellthru+'\n',
-                        // dataResult += 'id_outlet_st : '+item.id_outlet_st+'\n',
-                        // dataResult += 'no_rs : '+item.no_rs+'\n',
-                        // dataResult += 'nama_outlet : '+item.nama_outlet+'\n',
-                        // dataResult += 'kelurahan : '+item.kelurahan+'\n',
-                        // dataResult += 'kecamatan : '+item.kecamatan+'\n',
-                        // dataResult += 'kota : '+item.kota+'\n' 
+                      
                     })  
 
                     ctx.reply(dataResult);
@@ -512,15 +426,12 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
         let date = year+month;
 
         var sql1 = "select * from digipos_revamp_v2.report_nsb_"+date+" limit 1" ;
-        // var sql2 = "select * from digipos_revamp_v2.report_sellthru_"+date+" limit 1" ;
         var sql3 = "select * from digipos_revamp_v2.report_outlet_reference_"+date+" limit 1" ;
 
         dataSts['data1']  = await sqlCek(sql1,date);
-        // dataSts['data2']  = await sqlCek(sql2,date);
         dataSts['data3']  = await sqlCek(sql3,date);
 
         dateCek['date1']  = await getDate5(dataSts['data1']);
-        // dateCek['date2']  = await getDate5(dataSts['data2']);
 
         var dateCek1 = new Array(
             dateCek['date1'].date1,
@@ -530,13 +441,6 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
             dateCek['date1'].date5
         )
         
-        // var dateCek2 = new Array(
-        //     dateCek['date2'].date1,
-        //     dateCek['date2'].date2,
-        //     dateCek['date2'].date3,
-        //     dateCek['date2'].date4,
-        //     dateCek['date2'].date5
-        // )
 
         console.log("start setion 1")
 
@@ -560,34 +464,15 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
         if(dataSts['data5'] !== null){  
             console.log("data ditemukan");
 
-             // cek tbl 2 
-            // for (let i = 0; i < 5; i++) {    
-
-            //     console.log(dateCek2[i])  
-            //     var dtFix2 = dateCek2[i] 
-
-            //     var sql_cek = "select serial_number from (select * from digipos_revamp_v2.report_nsb_"+dtFix1+" where B_NUMBER="+dataInput+" limit 1)a LEFT JOIN (  select serial_number,CONCAT('62',(CAST(msisdn AS UNSIGNED))) msisdn1 from digipos_revamp_v2.report_sellthru_"+dateCek2[i]+"  ) b ON msisdn1=B_NUMBER " ;
-            //     dataSts['data4']  = await sqlCek1(sql_cek); 
-
-            //     if(dataSts['data4'] !== null){  
-            //         break;   
-            //     }  
-            // }  
         
-            // console.log(dtFix2)  
 
             var sql = "select B_NUMBER as No_MSISDN, CREATED_DATE as Tgl_trx, Price as Harga_paket, TYPE_STOCK, CHANNEL, PACKAGE_KEYWORD as Product_code, PRODUCT_L1,PRODUCT_L2,PRODUCT_L3 "+
-                    // ", OUTLET_ID, nama_outlet, RS as no_rs, sf_code as nama_sf, kelurahan,kecamatan, kota,  cluster, branch, regional, "+
-                    // "tgl_sellthru,sf_sellthru,id_outlet_st "+
-                    
+                   
                     "from (select * from digipos_revamp_v2.report_nsb_"+dtFix1+"  where B_NUMBER="+dataInput+" limit 1 )a "+
                     
                     "left join (select `BID ID` as Product_code1, PRODUCT_L1,PRODUCT_L2,PRODUCT_L3 from test.refference_bid_product_digipos) b ON PACKAGE_KEYWORD=Product_code1 ";
                     
-                    // "LEFT JOIN (select CONCAT('62',(CAST(msisdn AS UNSIGNED))) msisdn1,created_date as tgl_sellthru, outlet_id as id_outlet_st,sf_code as sf_sellthru from digipos_revamp_v2.report_sellthru_"+dtFix2+" ) c ON msisdn1=B_NUMBER "+
-                    
-                    // "LEFT JOIN (select no_rs, sf_code, outlet_id as otl_id, nama_outlet, kelurahan,kecamatan, kota, cluster, branch, regional, tgl_loading as tgl_otl from digipos_revamp_v2.report_outlet_reference_"+dataSts['data3']+")d ON otl_id=OUTLET_ID "+
-                    
+                   
                     "ORDER BY tgl_otl desc limit 1 ";
 
             conn.query(sql, function (err, result, fields){
@@ -612,19 +497,6 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
                         dataResult += 'PRODUCT_L1: '+item.PRODUCT_L1+'\n',
                         dataResult += 'PRODUCT_L2: '+item.PRODUCT_L2+'\n',
                         dataResult += 'PRODUCT_L3: '+item.PRODUCT_L3+'\n'
-                        // dataResult += 'OUTLET_ID: '+item.OUTLET_ID+'\n',
-                        // dataResult += 'nama_outlet: '+item.nama_outlet+'\n',
-                        // dataResult += 'no_rs: '+item.no_rs+'\n'
-                        // dataResult += 'nama_sf: '+item.nama_sf+'\n',
-                        // dataResult += 'kelurahan: '+item.kelurahan+'\n',
-                        // dataResult += 'kecamatan: '+item.kecamatan+'\n',
-                        // dataResult += 'kota: '+item.kota+'\n',
-                        // dataResult += 'cluster: '+item.cluster+'\n',
-                        // dataResult += 'branch: '+item.branch+'\n',
-                        // dataResult += 'regional: '+item.regional+'\n'
-                        // dataResult += 'tgl_sellthru: '+item.tgl_sellthru+'\n',
-                        // dataResult += 'sf_sellthru: '+item.sf_sellthru+'\n',
-                        // dataResult += 'id_outlet_st: '+item.id_outlet_st+'\n'
                     })  
  
                     result = dataResult
@@ -663,15 +535,12 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
         let date = year+month;
 
         var sql1 = "select * from hadoop.voucher_fisik_aktivasi_nasional_"+date+" limit 1" ;
-        // var sql2 = "select * from digipos_revamp_v2.report_sellthru_"+date+" limit 1" ;
         var sql3 = "select * from digipos_revamp_v2.report_outlet_reference_"+date+" limit 1" ;
 
         dataSts['data1']  = await sqlCek(sql1,date);
-        // dataSts['data2']  = await sqlCek(sql2,date);
         dataSts['data3']  = await sqlCek(sql3,date);
 
         dateCek['date1']  = await getDate5(dataSts['data1']);
-        // dateCek['date2']  = await getDate5(dataSts['data2']);
     
         var dateCek1 = new Array(
             dateCek['date1'].date1,
@@ -681,13 +550,6 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
             dateCek['date1'].date5
         )
         
-        // var dateCek2 = new Array(
-        //     dateCek['date2'].date1,
-        //     dateCek['date2'].date2,
-        //     dateCek['date2'].date3,
-        //     dateCek['date2'].date4,
-        //     dateCek['date2'].date5
-        // )
 
         console.log("start setion 1")
 
@@ -711,34 +573,13 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
         if(dataSts['data5'] !== null){  
             console.log("data ditemukan");
     
-                //cek tbl 2  
-            // for (let i = 0; i < 5; i++) {    
-
-            //     console.log(dateCek2[i])  
-            //     var dtFix2 = dateCek2[i] 
-    
-            //     var sql_cek = "select serial_number1 from (SELECT * FROM hadoop.voucher_fisik_aktivasi_nasional_"+dtFix1+" where serial_number="+dataInput+" limit 1)a LEFT JOIN (  select serial_number as serial_number1,msisdn as msisdn1 from digipos_revamp_v2.report_sellthru_"+dateCek2[i]+"  ) b ON serial_number1=serial_number where serial_number1 is not null " ;
-            //     dataSts['data4']  = await sqlCek1(sql_cek); 
-      
-            //     if(dataSts['data4'] !== null){  
-            //         console.log(dataSts['data4'])
-            //        break;   
-            //     } 
-            // }
-
-            // console.log(dtFix2)  
+        
     
             var sql = "select  "+
                      "serial_number as no_seri, event_date as tgl_trx, rech as harga_paket "+
-                    //  ", outlet_id as id_outlet, nama_outlet, no_rs, nama_sf, kelurahan,kecamatan, kota, cluster, branch, regional "+
-                    //  ",sf_sellthru, id_outlet_st "+
-                        
+                 
                      "from (SELECT * FROM hadoop.voucher_fisik_aktivasi_nasional_"+dtFix1+" where serial_number="+dataInput+" limit 1) a ";
-                        
-                    //  "LEFT JOIN (select CONCAT('62',(CAST(msisdn AS UNSIGNED))) msisdn1, serial_number as serial_number1,created_date as tgl_sellthru, outlet_id as id_outlet_st,sf_code as sf_sellthru from digipos_revamp_v2.report_sellthru_"+dtFix2+" ) c ON serial_number1=serial_number "+
-                        
-                    //  "LEFT JOIN (select no_rs, sf_code as nama_sf, outlet_id as otl_id, nama_outlet, kelurahan,kecamatan, kota, tgl_loading as tgl_otl from digipos_revamp_v2.report_outlet_reference_"+dataSts['data3']+" )d ON otl_id=OUTLET_ID "+
-                        
+                 
                      "ORDER BY tgl_otl desc limit 1 "; 
     
             conn.query(sql, function (err, result, fields){
@@ -758,17 +599,6 @@ bot.start(async (ctx) => { ctx.reply('Selamat Datang '+ctx.from.first_name); })
                         dataResult += 'tgl_trx: '+ item.tgl_trx+'\n',
                         dataResult += 'harga_paket: '+ item.harga_paket+'\n',
                         dataResult += 'id_outlet: '+ item.id_outlet+'\n'
-                        // dataResult += 'nama_outlet: '+ item.nama_outlet+'\n',
-                        // dataResult += 'no_rs: '+ item.no_rs+'\n',
-                        // dataResult += 'nama_sf: '+ item.nama_sf+'\n',
-                        // dataResult += 'kelurahan: '+ item.kelurahan+'\n',
-                        // dataResult += 'kecamatan: '+ item.kecamatan+'\n',
-                        // dataResult += 'kota: '+ item.kota+'\n',
-                        // dataResult += 'cluster: '+ item.cluster+'\n',
-                        // dataResult += 'branch: '+ item.branch+'\n',
-                        // dataResult += 'regional: '+ item.regional+'\n'
-                        // dataResult += 'sf_sellthru: '+ item.sf_sellthru+'\n',
-                        // dataResult += 'id_outlet_st: '+ item.id_outlet_st+'\n'
                     })  
     
                     ctx.reply(dataResult);
